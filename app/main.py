a932ddb94d737868381cf68c6eeaec091f33f818
Iuri-alt <iuri3456@outlook.com>
2026-04-19 21:28:50 -0300

    fix: dashboard import

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from app.database import Base, engine
-from app.routes import  auth, expenses, dashboard
+from app.routes import  auth, expenses
 
 Base.metadata.create_all(bind=engine)
 
@@ -8,4 +8,3 @@ app = FastAPI(title="Entrix API")
 
 app.include_router(auth.router, prefix="/auth", tags=["Auth"])
 app.include_router(expenses.router, prefix="/expenses", tags=["Expenses"])
-app.include_router(dashboard.router, prefix="/dashboard", tags=["Dashboard"])
